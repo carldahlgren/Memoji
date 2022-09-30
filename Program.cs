f0fa8fd355b2d70f40783de33c7ff12dc5dba963
carldahlgren <83665107+carldahlgren@users.noreply.github.com>
2022-09-30 11:30:53 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Projekt{
+class Mainclass{
+    public static void Main (string [] args){
+        string name = args [0];
+        System.Console.WriteLine("Tjena, " + name + "!! " + "Detta är projektmappen");
+        
+    }
+}
+
+}
